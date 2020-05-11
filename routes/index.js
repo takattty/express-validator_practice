cc676caea1cb65640d2dfafc868acb78d67e4c0f
takattty <r9r_celtics_t9t@icloud.com>
2020-05-11 13:52:45 +0900

    created base function

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/submit', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+})
+
 module.exports = router;
