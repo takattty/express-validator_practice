bf37e1e8849535d85fc8c254f0900430cb051075
takattty <r9r_celtics_t9t@icloud.com>
2020-05-19 20:06:02 +0900

    seted validationRouter

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const { body, validationResult } = require('express-validator');
-//const validationCheck = require('../validation');
+const validationCheck = require('../validation');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/submit', validation(), async (req, res, next) => {
+router.post('/submit', validationCheck, async (req, res, next) => {
   const error = validationResult(req);
   if(!error.isEmpty()){
     console.log(error.errors);
@@ -16,24 +16,7 @@ router.post('/submit', validation(), async (req, res, next) => {
   const name = req.body.user_name;
   const password = req.body.user_pass;
   const text = req.body.user_message;
-  console.log(name);
-
-
   res.redirect('/');
 })
-
-function validation() {
-  return [
-      body('user_name').escape(),//エスケープ処理
-      body('user_name').trim(),
-      body('user_name').not().isEmpty(),//空でない
-      body('user_name').contains(),
-      body('user_name').isLength({ max:20 }).withMessage('too long!'),
-      body('user_pass').escape(),
-      body('user_pass').not().isEmpty(),
-      body('user_pass').isLength({ max: 20 }),
-      body('user_message').not().isEmpty()
-    ]
-}
 
 module.exports = router;
--- validation.js
+++ validation.js
@@ -1,10 +1,17 @@
 'use strict';
 const { body } = require('express-validator');
 
-// const validation = function validation() {
-//   return [
+const validationCheck = [
+  body('user_name').escape(),//エスケープ処理
+  body('user_name').trim(),
+  body('user_name').not().isEmpty(),//空でない
+  body('user_name').contains(),
+  body('user_name').isLength({ max:20 }).withMessage('too long!'),
+  body('user_pass').escape(),
+  body('user_pass').not().isEmpty(),
+  body('user_pass').isLength({ max: 20 }),
+  body('user_message').not().isEmpty()
+]
 
-//   ]
-// }
-
-// module.exports = validation;
+module.exports = validationCheck;
+//exportのみじゃ、配列をエクスポート出来なかった。
